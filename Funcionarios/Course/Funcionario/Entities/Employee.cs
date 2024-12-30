6fa714b6135f61dea6601ffcd6756c74a60176cc
Fernanda de Aquino Marques <156463611+FernandaMarques030321@users.noreply.github.com>
2024-12-30 10:47:30 -0300

    Add files via upload

---
+++ Funcionarios/Course/Funcionario/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Course.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuesPerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuesPerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuesPerHour = valuesPerHour;
+        }
+        public virtual double Payment()
+        {
+            return Hours * ValuesPerHour;
+        }
+    }
+}
---
+++ Funcionarios/Course/Funcionario/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,20 @@
+﻿namespace Course.Entities
+{
+    class OutsourcedEmployee : Employee
+    { 
+        public double AdditionalCharge { get; set; }
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuesPerHour, double additionalCharge)
+           : base (name, hours,valuesPerHour)
+        {
+            AdditionalCharge = additionalCharge;
+
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 *AdditionalCharge;
+        }
+    }
+}
