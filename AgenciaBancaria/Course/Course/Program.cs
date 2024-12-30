77efe28b2709b50a6150da7699a035350d3fd9b0
Fernanda de Aquino Marques <156463611+FernandaMarques030321@users.noreply.github.com>
2024-12-30 10:26:48 -0300

    Add files via upload

---
+++ AgenciaBancaria/Course/Course/Program.cs
@@ -0,0 +1,46 @@
+﻿﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+           
+            Console.Write("Entre com o nome do titular da conta: ");
+            string titularConta = Console.ReadLine();
+        
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numeroConta, titularConta, depositoInicial);
+            }
+            else {
+                conta = new ContaBancaria(numeroConta, titularConta);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
